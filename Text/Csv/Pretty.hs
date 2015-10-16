602f450563e88733df92109da61c1a702b10bc5b
Franz Thoma <franz.thoma@tngtech.com>
2015-10-16 18:45:54 +0200

    Prefer breaking at word boundaries

--- Text/Csv/Pretty.hs
+++ Text/Csv/Pretty.hs
@@ -9,6 +9,7 @@ import qualified Data.List as L
 import           Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy.IO as T
 import qualified Data.Text.Lazy as T
+import           Data.Monoid
 
 import Text.Csv.Types
 
@@ -46,6 +47,19 @@ alignCol cells = fmap (pad maxLength) cells
 
 breakLong :: Int -> Text -> [Text]
 breakLong lim s = if T.length s >= fromIntegral lim
-    then let (s', rest) = T.splitAt (fromIntegral lim) s
+    then let (s', rest) = breakWords s
          in  s' : breakLong lim rest
     else [s]
+  where
+    breakWords :: Text -> (Text, Text)
+    breakWords s = let (l, r) = forceBreak s
+                       (l1, r1) = spanEnd (not . (`elem` wordBoundaries)) l
+                   in  if T.null l1 then (l, r) else (l1, r1 <> r)
+
+    wordBoundaries = [',',' ']
+    spanEnd :: (Char -> Bool) -> Text -> (Text, Text)
+    spanEnd p s = let (r, l) = T.span p (T.reverse s)
+                  in  (T.reverse l, T.reverse r)
+
+    forceBreak :: Text -> (Text, Text)
+    forceBreak = T.splitAt (fromIntegral lim)
